feb3ff8e993fb22f98bf7b71767e44b244545f71
GerardAguilar <gerard.aguilar.156@my.csun.edu>
2016-12-31 15:21:45 -0800

    Level, basic movement, sound, dust trails

--- Assets/Scripts/Tank/TankMovement.cs
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -10,7 +10,7 @@ public class TankMovement : MonoBehaviour
     public AudioClip m_EngineDriving;      
     public float m_PitchRange = 0.2f;
 
-    /*
+    
     private string m_MovementAxisName;     
     private string m_TurnAxisName;         
     private Rigidbody m_Rigidbody;         
@@ -46,34 +46,70 @@ public class TankMovement : MonoBehaviour
 
         m_OriginalPitch = m_MovementAudio.pitch;
     }
-    */
+    
 
     private void Update()
     {
         // Store the player's input and make sure the audio for the engine is playing.
+        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+
+        EngineAudio();
     }
 
 
     private void EngineAudio()
     {
         // Play the correct audio clip based on whether or not the tank is moving and what audio is currently playing.
+        if (Mathf.Abs(m_MovementInputValue) < 0.1f && Mathf.Abs(m_TurnInputValue) < 0.1f) {
+            //movement and turn inputs range from -1 to 1, so getting the absolute value senses any form of movement/turning
+            //So if, barely moving, play idling
+            if (m_MovementAudio.clip == m_EngineDriving) {
+                m_MovementAudio.clip = m_EngineIdling;
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
+            }
+
+        }
+        else {//otherwise, should be moving
+            if (m_MovementAudio.clip == m_EngineIdling)
+            {
+                m_MovementAudio.clip = m_EngineDriving;
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
+            }
+        }
+
+
     }
 
 
     private void FixedUpdate()
     {
         // Move and turn the tank.
+        Move();
+        Turn();
     }
 
 
     private void Move()
     {
         // Adjust the position of the tank based on the player's input.
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;//Time.deltaTime makes the movment proportional to the second, instead of each frame.
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);// A movement based on absolute coordinates, made relative by including the rb's position.
+
+
     }
 
 
     private void Turn()
     {
         // Adjust the rotation of the tank based on the player's input.
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;//turn value in degrees
+
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);//Quaternion.Euler turns a Vector3 into a Quaternion.
+
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation); //Apply the rotation to the body
     }
 }
