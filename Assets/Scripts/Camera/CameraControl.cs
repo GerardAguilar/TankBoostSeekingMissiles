0169a012582fbb65628ae9960a56e2318602e6ef
GerardAguilar <gerard.aguilar.156@my.csun.edu>
2017-01-01 19:55:52 -0800

    Camera Move and Zoom

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -5,7 +5,7 @@ public class CameraControl : MonoBehaviour
     public float m_DampTime = 0.2f;                 
     public float m_ScreenEdgeBuffer = 4f;           
     public float m_MinSize = 6.5f;                  
-    [HideInInspector] public Transform[] m_Targets; 
+    /*[HideInInspector]*/ public Transform[] m_Targets; 
 
 
     private Camera m_Camera;                        
@@ -45,16 +45,16 @@ public class CameraControl : MonoBehaviour
             if (!m_Targets[i].gameObject.activeSelf)
                 continue;
 
-            averagePos += m_Targets[i].position;
+            averagePos += m_Targets[i].position;//accrue all positions
             numTargets++;
         }
 
         if (numTargets > 0)
-            averagePos /= numTargets;
+            averagePos /= numTargets;//average out the accrued positions
 
-        averagePos.y = transform.position.y;
+        averagePos.y = transform.position.y;//change y to current camera's y (don't want average of y's)
 
-        m_DesiredPosition = averagePos;
+        m_DesiredPosition = averagePos;//change the global m_DesiredPosition var to be SmoothDamped in the next Move() line.
     }
 
 
@@ -65,9 +65,9 @@ public class CameraControl : MonoBehaviour
     }
 
 
-    private float FindRequiredSize()
+    private float FindRequiredSize()//zoom to accomodate furthest tank
     {
-        Vector3 desiredLocalPos = transform.InverseTransformPoint(m_DesiredPosition);
+        Vector3 desiredLocalPos = transform.InverseTransformPoint(m_DesiredPosition);//uses Camera Rig's local space
 
         float size = 0f;
 
@@ -76,29 +76,29 @@ public class CameraControl : MonoBehaviour
             if (!m_Targets[i].gameObject.activeSelf)
                 continue;
 
-            Vector3 targetLocalPos = transform.InverseTransformPoint(m_Targets[i].position);
+            Vector3 targetLocalPos = transform.InverseTransformPoint(m_Targets[i].position);//again, relative to the camera's local space
 
             Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
 
-            size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.y));
+            size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.y));//calculate vertical separately
 
-            size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.x) / m_Camera.aspect);
+            size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.x) / m_Camera.aspect);//compare horizontal with earlier vertical
         }
         
         size += m_ScreenEdgeBuffer;
 
-        size = Mathf.Max(size, m_MinSize);
+        size = Mathf.Max(size, m_MinSize);//Is the minSize bigger than the current biggest?
 
         return size;
     }
 
 
-    public void SetStartPositionAndSize()
+    public void SetStartPositionAndSize()//to be used at the beginning of each fight
     {
-        FindAveragePosition();
+        FindAveragePosition();//Just Move w/o the dampening
 
         transform.position = m_DesiredPosition;
 
-        m_Camera.orthographicSize = FindRequiredSize();
+        m_Camera.orthographicSize = FindRequiredSize();//Just Zoom w/o the dampening
     }
 }
