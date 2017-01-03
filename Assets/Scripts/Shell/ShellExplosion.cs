92c7d3c84de8d90e7de9cc4c8c510f2c7c89e384
GerardAguilar <gerard.aguilar.156@my.csun.edu>
2017-01-02 22:42:13 -0800

    Shell bullet prefab, has knockback!

--- Assets/Scripts/Shell/ShellExplosion.cs
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -10,7 +10,6 @@ public class ShellExplosion : MonoBehaviour
     public float m_MaxLifeTime = 2f;                  
     public float m_ExplosionRadius = 5f;              
 
-
     private void Start()
     {
         Destroy(gameObject, m_MaxLifeTime);
@@ -20,12 +19,52 @@ public class ShellExplosion : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // Find all the tanks in an area around the shell and damage them.
+        //Similar to raycast, any thing inside sphere would be collected.
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);//origin, radius, and what layers to collect
+
+        for (int i = 0; i < colliders.Length; i++) {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+            if (!targetRigidbody){
+                continue;
+            }
+            targetRigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);//explode at position of bullet
+
+            TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
+
+            if (!targetHealth) {
+                continue;
+            }
+
+            float damage = CalculateDamage(targetRigidbody.position);
+
+            targetHealth.TakeDamage(damage);
+        }
+
+        m_ExplosionParticles.transform.parent = null; //Detach components to become separate from destroyed shell.
+        m_ExplosionParticles.Play();
+
+        m_ExplosionAudio.Play();
+
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.duration);//duration of the explosion
+
+        Destroy(gameObject);
     }
 
 
     private float CalculateDamage(Vector3 targetPosition)
     {
         // Calculate the amount of damage a target should take based on it's position.
-        return 0f;
+        Vector3 explosionToTarget = targetPosition - transform.position;
+
+        float explosionDistance = explosionToTarget.magnitude;
+
+        float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;//should be between 0 and 1.
+        //the smaller the explosionDistance, the bigger the m_ExplosionRadius
+
+        float damage = relativeDistance * m_MaxDamage;//0% - 100% or max damage
+
+        damage = Mathf.Max(0f, damage);//accounts for negative damage values
+
+        return damage;
     }
 }
