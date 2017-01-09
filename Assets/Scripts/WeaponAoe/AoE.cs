9e242ce511234f9f2bd1d357a92cf48d17306e09
GerardAguilar <gerard.aguilar.156@my.csun.edu>
2017-01-09 00:22:44 -0800

    Filling/Resetting Target Array for Weapon System

---
+++ Assets/Scripts/WeaponAoe/AoE.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AoE : MonoBehaviour {
+
+    public Vector3 target;
+    public bool targetInRange;
+
+    string playerNum;
+    TankShooting tankShooting;
+    WeaponSystem weaponSystem;
+
+	// Use this for initialization
+	void Awake () {
+        weaponSystem = GetComponentInParent<WeaponSystem>();
+        playerNum = weaponSystem.playerNum;
+        tankShooting = GetComponentInParent<TankShooting>();
+	}
+
+    public void OnTriggerEnter(Collider other) {
+        //place in weapon system target array
+        if (IsEnemy(other))
+        {
+            if (IsNewEnemy(other))
+            {
+                weaponSystem.AddTarget(other.gameObject);
+            }
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        //turn on the targetting UI of opposing enemy
+        //track enemy's position
+        targetInRange = IsEnemy(other);
+        if (targetInRange)
+        {
+            //StartCoroutine(LoadMissile(other));
+            LoadMissile(other);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        //remove from weapon system target array
+    }
+
+    public void OnDisable() {
+        target = new Vector3();
+        weaponSystem.ClearTargetArray();
+        //flush out Weapon System's target Array
+    }
+
+    public bool IsEnemy(Collider other)
+    {
+
+        bool itIsEnemy = false;
+        if (!other.gameObject.CompareTag("Player"))
+        {
+            itIsEnemy = false;
+        }
+        else if (other.GetComponent<TankShooting>().m_PlayerNumber != tankShooting.m_PlayerNumber)
+        {
+            itIsEnemy = true;
+        }
+
+        return itIsEnemy;
+    }
+
+    //IEnumerator LoadMissile(Collider other)
+    //{
+    //    //Debug.Log("Loading"+playerNum);
+    //    //Debug.Log("Targeting: " + other.transform.position);
+    //    target = other.transform.position;
+    //    yield return new WaitForSeconds(10f);
+    //}
+
+    void LoadMissile(Collider other)
+    {
+        //Debug.Log("Loading"+playerNum);
+        //Debug.Log("Targeting: " + other.transform.position);
+        target = other.transform.position;
+    }
+
+    bool IsNewEnemy(Collider other) {
+        //if the enemy is already in this AoE's array, then don't worry
+        bool itIsNewEnemy = false;
+        if (!weaponSystem.IsTargetInArray(other.gameObject)) {
+            itIsNewEnemy = true;
+        }
+        return itIsNewEnemy;
+    }
+
+    
+}
---
+++ Assets/Scripts/WeaponAoe/WeaponSystem.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class WeaponSystem : MonoBehaviour {
+
+    public string playerNum;
+    public TankShooting tankShooting;
+
+    public Rigidbody missile;
+    public Transform fireTransform;
+    public List<GameObject> targetObjects;
+
+
+    private GameObject aoe;
+    private AoE aoeScript;
+    //private Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+        tankShooting = GetComponentInParent<TankShooting>();
+        fireTransform = tankShooting.m_FireTransform;
+        playerNum = tankShooting.m_PlayerNumber + "";
+        aoe = tankShooting.aoe;
+        aoeScript = aoe.GetComponent<AoE>();
+        aoe.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Missile" + playerNum))
+        {
+            //this is where we aim and collect
+            Debug.Log("Missile" + playerNum + " was pressed.");
+            aoe.SetActive(true);
+        }
+        else if (Input.GetButtonUp("Missile" + playerNum)) {
+            //This is also where we launch the missiles
+            Debug.Log("Target: " + aoeScript.target);
+            //if (aoeScript.IsEnemy()) { 
+                FireMissile(aoeScript.target);
+            //}
+            aoe.SetActive(false);
+        }
+    }
+
+    void FireMissile(Vector3 target) {
+        Debug.Log("Fire Missile at " + target);
+
+        //Need to instantiate missile
+        Rigidbody shellInstance = Instantiate(missile, fireTransform.position, fireTransform.rotation) as Rigidbody;
+
+        //Lerp the position
+        shellInstance.velocity = 5f * fireTransform.forward;
+
+        //the lock on should be time-limited
+    }
+
+    public void AddTarget(GameObject target) {
+        targetObjects.Add(target);
+    }
+
+    public bool IsTargetInArray(GameObject target) {
+        bool targetIsInArray = false;
+
+        for (int i = 0; i < targetObjects.Count; i++) {
+            if (target == targetObjects[i]) {
+                targetIsInArray = true;
+                break;
+            }
+        }
+
+        return targetIsInArray;
+    }
+
+    public void ClearTargetArray() {
+        this.targetObjects = new List<GameObject>();
+        
+    }
+
+}
